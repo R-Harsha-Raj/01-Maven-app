b481d2bad93a468b866f4197b7e3512f32f2c2b4
R-Harsha-Raj <17311A05V8@sreenidhi.edu.in>
2025-07-27 11:42:43 +0530

    added sum

--- 1-Maven-app/src/main/java/mvn/Demo/App.java
+++ 1-Maven-app/src/main/java/mvn/Demo/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World, I am being compiled by Maven and pushed by Git!!!!" );
+        System.out.println("Hello World, I am being compiled by Maven and pushed by Git!!!!");
+        int a = 30;
+        int b = 40;
+        int c = a + b;
+        System.out.println("Sum of a + b is :" + c);
     }
 }
