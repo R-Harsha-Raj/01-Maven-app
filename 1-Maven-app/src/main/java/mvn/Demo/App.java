537888cc7689678fcdd65ed780d8925ac23e7cac
R-Harsha-Raj <17311A05V8@sreenidhi.edu.in>
2025-07-27 11:15:08 +0530

    first edit

--- 1-Maven-app/src/main/java/mvn/Demo/App.java
+++ 1-Maven-app/src/main/java/mvn/Demo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World, I am being compiled by Maven and pushed by Git!!!!" );
     }
 }
